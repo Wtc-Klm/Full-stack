6b89671384e4af9f06cb3dc0e2de831d4dc17b2b
Wtc-Klm <incomet2004@gmail.com>
2025-12-02 15:18:54 +0700

    1

--- Week-2/09_Date,math,string.js
+++ Week-2/09_Date,math,string.js
@@ -11,8 +11,3 @@ function createLogENtry(massage)
 const log = createLogENtry('User login successfully');
 console.log(log);
 // ตัวอย่างผลลัพธ์: [14:35:20] [A1B2C3D4] USER LOGIN SUCCESSFULLY
-<<<<<<< HEAD
-test
-=======
-10
->>>>>>> cf6e89d30e1f59be1a049b73b454c4f90fc52e20
