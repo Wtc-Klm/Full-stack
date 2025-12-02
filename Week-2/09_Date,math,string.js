51b4a4c360a387ef1d4bc9279938ac63cd2a6f33
Wtc-Klm <incomet2004@gmail.com>
2025-12-02 15:13:26 +0700

    End class

---
+++ Week-2/09_Date,math,string.js
@@ -0,0 +1,12 @@
+function createLogENtry(massage)
+{
+    const now = new Date();
+    const timestamp = now.toLocaleTimeString('th-TH')
+
+    const eventId = Math.random().toString(16).substring(2,10).toUpperCase();
+    const logMassage = massage.toUpperCase();
+
+    return `[${timestamp}] [${eventId}] ${logMassage}`;
+}
+const log = createLogENtry('User login successfully');
+console.log(log);
