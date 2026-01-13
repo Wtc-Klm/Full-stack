06431732e050a748c709d5426896f840f6de5287
Wtc-Klm <incomet2004@gmail.com>
2026-01-13 13:53:45 +0700

    1/13/26

---
+++ Week-6/00_.js
@@ -0,0 +1,13 @@
+const promise = new Promise((resolve, reject) => {
+  const res = true;
+  if (res){
+    resolve("Resolved!");
+  } else {
+    reject("Rejected!");
+  }
+});
+
+promise.then(
+    (res) => console.log(res),
+    (err) => console.log(err)
+);
